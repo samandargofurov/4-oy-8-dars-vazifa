824d7f26f98461a2f6516d3af18518d9f8afaf70
samandargofurov <samandargofurov236@gmail.com>
2023-12-23 19:02:17 +0500

    new lesson

---
+++ dars uchun/index.js
@@ -0,0 +1,23 @@
+// let array = [1, 2, 3, 4, 5, 6];
+
+// for (const value of array){
+//     console.log(value);
+// }
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+
+// let a = arr.find((value) => {
+//     return value > 4
+// });
+
+// console.log(a);
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+
+// let a = arr.findIndex((value) => {
+//     return value >= 4
+// });
+
+// console.log(a);
---
+++ index.js
@@ -0,0 +1,9 @@
+// 1-masala
+// Massiv ichidan berilgan qiymatni qidiradigan agar mavjud bolsa uning indexlarining kvadratini aks holda mavjud emas degan yozuvni qaytaradigan funksiya yozing (IndexOf).
+
+// 2-masala
+// 3 ta massivni birlashtiradigan birinchi massivni oxirida, 2-sini boshida va 1-sini ortasida qoyib beradigan funksiya yozing. (concat)
+
+
+// 3-masala
+// Sonlardan tashkil topgan massivning ichidan shartga mos dastlabki elementning kvadratini qaytaradigan funksiya yozing (find)
